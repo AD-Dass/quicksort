a77e851b8a91529a6623ea522b052da3df6a0967
Araan <5108521+AD-Dass@users.norepky.github.com>
2021-02-20 23:49:26 +0000

    origin

--- sortinghat.py
+++ sortinghat.py
@@ -1,15 +1,13 @@
 import math
 import sys
 
-num=[1]
-
-def quick (numbers):
+def quick(numbers):
     if len(numbers)>1:
         pivot = numbers.pop(-1)
         left=[]
         right=[]
         for x in range(0,len(numbers),1):
-            if numbers[x]>=pivot:
+            if numbers[x]<=pivot:
                 left.append(numbers[x])
             else:
                 right.append(numbers[x])
@@ -19,5 +17,6 @@ def quick (numbers):
 
 
 if __name__=='__main__':
-    numbers=sys.argv[1]
-    quick(numbers)
+    numbers=[7,3,6,3,2,3,7,8] #change to a list when doing internal sorting [7,3,6,3,2,3,7,8] or sys.argv[1]
+    new_numbers=quick(numbers)
+    print(new_numbers)
